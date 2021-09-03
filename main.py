dcfbb1f82a41658163121c068349cbc5a4a91dd5
divos <divos.patrick@gmail.com>
2021-09-04 01:28:01 +0200

    Database connection and Data validation

--- main.py
+++ main.py
@@ -9,9 +9,41 @@ import sqlite3
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER_ID = "f6frc1m72da9a2r0chb3vj85w"
-TOKEN = "BQATo3HuR0i7C4D_jxcHYEFOrz3tDK7SpDpTMw4GmKvVZ5cY0gqgHvFE4rDFdJBOLYjc6oMKg57tucZn9PTFJkfY1G9AaCpHkEkhrbMQcWQEds1XV2z6q1wSFwvWIc84PbsKcqRpD4JMnRvDXI3M4gxXOkJbKnJrIR9eLFPK"
+# get token from: https://developer.spotify.com/console/get-recently-played/?limit=&after=&before=
+TOKEN = "BQCvwZmJXsKxYb9gMhHLiC_HQDmudibwRP4Kec8tnQ9l9qIEf9ZhCjpvhuRcDmve8_h2E8wsR0EfiY_lh62xDfhZkc9a5C48PwwWL_qcLYnb2J3IVmW6lu5xWpPko5LYkUVRgKPrfv5kOnni9ZMwFzjyfPThxnaOn-t-YaJZ"
+
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if DataFrame is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False
+
+    #Primary Key Check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key Check is violated")
+
+    #check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null value Found")
+
+    # Check that timestamps are of yesterdays date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0,minute=0,second=0,microsecond=0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
+            raise Exception("At least one of the returned songs is not from within the last 24 hours")
+
+    return True
+
+
 
 if __name__ == "__main__":
+
+    # Extraction of Data
 
     headers = {
         "Accept": "application/json",
@@ -51,4 +83,32 @@ if __name__ == "__main__":
 
     print(song_df)
     
-    
+    # Validate
+    if check_if_valid_data(song_df):
+        print("Data is valid, proceed to Load stage")
+
+    # Load to DB
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    print("Established DB connection")
+
+    try: 
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the Database")
+
+    conn.close()
+    print("closed DB connection")
